b76613b9d4fb4a2bbb5dcf50e2323f4853b9c042
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2021-04-25 16:00:55 +0100

    Handle case when no matches found

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ function UL(array) {
   `
 }
 
+function errorMessage(value) {
+  return `<p>Sorry, no countries were found matching your search term "${value}".`
+}
+
 function renderResults(markup) {
   const $searchResults = document.querySelector('#search-results');
   $searchResults.innerHTML = markup;
@@ -20,17 +24,27 @@ function onSubmit(event) {
   event.preventDefault();
 
   const value = event.srcElement[0].value;
-  console.log(value);
 
   const $search = document.querySelector('#search');
   $search.reset();
 
-  // TODO: Gracefully handle 404s.
   nameSearch(value)
-    .then(countries => countries.map(country => country.name))
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Response not ok, status ${response.status}.`)
+      }
+    })
+    .then(countries => countries && countries.map(country => country.name))
     .then(UL)
     .then(renderResults)
-    .catch(e => console.error(e));
+    .catch(() => handleError(value));
+}
+
+function handleError(value) {
+  // TODO: More reasons this could go wrong than just no results,
+  // e.g. API down would error even if you searched "United".
+  const markup = errorMessage(value);
+  renderResults(markup);
 }
 
 const $search = document.querySelector('#search');
